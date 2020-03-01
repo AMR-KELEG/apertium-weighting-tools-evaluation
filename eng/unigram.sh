78e67f9dda2b2d753536892ae28a9d1e65a545fc
Amr Keleg <amr_mohamed@live.com>
2020-03-01 13:15:20 +0200

    Add the evaluation scripts for unigram method

---
+++ eng/unigram.sh
@@ -0,0 +1,21 @@
+LANG="eng"
+TAGGED_CORPUS="../../apertium-${LANG}/texts/eng.tagged"
+BIN="../../apertium-${LANG}/${LANG}.automorf.bin"
+FOLDS=$(mktemp -d)
+CLEANED_CORPUS=$(mktemp)
+UNI_LAPLACE=$(mktemp -d)
+UNI_SGT=$(mktemp -d)
+RESULTS_FILE="results/uni"
+cat "${TAGGED_CORPUS}" | apertium-cleanstream -n > "${CLEANED_CORPUS}"
+
+corpus_split.py "${CLEANED_CORPUS}" -n 5 -o "${FOLDS}"
+unigram_fit.py -i "${FOLDS}" -b "${BIN}" -o "${UNI_LAPLACE}"
+unigram_fit_sgt.py -i "${FOLDS}" -b "${BIN}" -o "${UNI_SGT}"
+
+
+echo "UNIGRAM LAPLACE" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${UNI_LAPLACE}" >> "${RESULTS_FILE}"
+echo "UNIGRAM SGT" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${UNI_SGT}" >> "${RESULTS_FILE}"
+
+rm -r "${CLEANED_CORPUS}" "$FOLDS" "${UNI_LAPLACE}" "${UNI_SGT}"
