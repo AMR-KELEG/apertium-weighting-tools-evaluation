eba17d83d15a3349c990458d11e29a07d458e270
Amr Keleg <amr_mohamed@live.com>
2020-03-01 20:12:05 +0200

    Add the evaluation scripts for random method

---
+++ eng/rand.sh
@@ -0,0 +1,23 @@
+LANG="eng"
+TAGGED_CORPUS="../../apertium-${LANG}/texts/eng.tagged"
+BIN="../../apertium-${LANG}/${LANG}.automorf.bin"
+FOLDS=$(mktemp -d)
+CLEANED_CORPUS=$(mktemp)
+UNTAGGED_CORPUS=$(mktemp)
+RAND_LAPLACE=$(mktemp -d)
+RAND_SGT=$(mktemp -d)
+RESULTS_FILE="results/rand"
+cat "${TAGGED_CORPUS}" | apertium-cleanstream -n > "${CLEANED_CORPUS}"
+
+cat ../../apertium-eng/texts/old/*.raw.txt > "${UNTAGGED_CORPUS}"
+
+corpus_split.py "${CLEANED_CORPUS}" -n 5 -o "${FOLDS}"
+random_fit.py -i "${FOLDS}" -b "${BIN}" -corpus "${UNTAGGED_CORPUS}" -o "${RAND_LAPLACE}"
+random_fit_sgt.py -i "${FOLDS}" -b "${BIN}" -corpus "${UNTAGGED_CORPUS}" -o "${RAND_SGT}"
+
+echo "RAND LAPLACE" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${RAND_LAPLACE}" >> "${RESULTS_FILE}"
+echo "RAND SGT" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${RAND_SGT}" >> "${RESULTS_FILE}"
+
+rm -r "${CLEANED_CORPUS}" "${UNTAGGED_CORPUS}" "$FOLDS" "${RAND_LAPLACE}" "${RAND_SGT}"
