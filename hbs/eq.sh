af5b6b782b9b5dc21de35549508101eceee3ecc9
Amr Keleg <amr_mohamed@live.com>
2020-03-02 14:33:26 +0200

    Add the evaluation scripts for equal and analysis length methods

---
+++ hbs/eq.sh
@@ -0,0 +1,16 @@
+LANG="hbs"
+TAGGED_CORPUS="../../apertium-${LANG}/hbs-tagger-data/hbs.tagged"
+BIN="../../apertium-${LANG}/${LANG}.automorf.bin"
+FOLDS=$(mktemp -d)
+CLEANED_CORPUS=$(mktemp)
+EQ=$(mktemp -d)
+RESULTS_FILE="results/eq"
+cat "${TAGGED_CORPUS}" | apertium-cleanstream -n > "${CLEANED_CORPUS}"
+
+corpus_split.py "${CLEANED_CORPUS}" -n 5 -o "${FOLDS}"
+equalweight_fit.py -i "${FOLDS}" -b "${BIN}" -o "${EQ}"
+
+echo "EQ" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${EQ}" >> "${RESULTS_FILE}"
+
+rm -r "${CLEANED_CORPUS}" "$FOLDS" "${EQ}"
---
+++ hbs/len.sh
@@ -0,0 +1,16 @@
+LANG="hbs"
+TAGGED_CORPUS="../../apertium-${LANG}/hbs-tagger-data/hbs.tagged"
+BIN="../../apertium-${LANG}/${LANG}.automorf.bin"
+FOLDS=$(mktemp -d)
+CLEANED_CORPUS=$(mktemp)
+LEN=$(mktemp -d)
+RESULTS_FILE="results/len"
+cat "${TAGGED_CORPUS}" | apertium-cleanstream -n > "${CLEANED_CORPUS}"
+
+corpus_split.py "${CLEANED_CORPUS}" -n 5 -o "${FOLDS}"
+analysis_length_fit.py -i "${FOLDS}" -b "${BIN}" -o "${LEN}"
+
+echo "LEN" >> "${RESULTS_FILE}"
+metrics_report.py -i "${FOLDS}" -b "${LEN}" >> "${RESULTS_FILE}"
+
+rm -r "${CLEANED_CORPUS}" "$FOLDS" "${LEN}"
